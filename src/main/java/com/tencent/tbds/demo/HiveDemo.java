40c2997cf397178176e04d2c60f26b8f0a732dfa
benhuachen <chenbenhua@gmail.com>
2019-01-24 15:24:40 +0800

    first commit

---
+++ src/main/java/com/tencent/tbds/demo/HiveDemo.java
@@ -0,0 +1,30 @@
+package com.tencent.tbds.demo;
+
+import java.sql.*;
+
+public class HiveDemo {
+    public static void main(String args[]) {
+        Connection conn;
+        try {
+            Class.forName("org.apache.hive.jdbc.HiveDriver");
+
+//            conn = DriverManager.getConnection("jdbc:hive2://tbds-10-3-0-13:10000/default", "bhchen", "bhchen_123");
+
+            //高可用方式：客户端字段选择可用的hiveserver
+            conn = DriverManager.getConnection("jdbc:hive2://tbds-10-3-0-13:2181,tbds-10-3-0-17:2181,tbds-10-3-0-7:2181/default;" +
+                            "serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2",
+                    "bhchen", "bhchen_123");
+            Statement st = conn.createStatement();
+            String sqlstring = "SHOW DATABASES";
+            ResultSet rs = st.executeQuery(sqlstring);
+            System.out.println("Show all databases in hive:");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
